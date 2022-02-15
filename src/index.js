e8ea9602d6746d86c3c26f591da89cce23b15d4e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-15 20:21:45 +0700

    Cleanup `reportWebVitals.js`

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./tests/reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
