d6e2ea27631b1c1af3e891f043c8d8c7b3ac2c17
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-16 20:50:13 +0700

    Create `BGText` component

--- src/components/BGText/BGText.js
+++ src/components/BGText/BGText.js
@@ -1,4 +1,6 @@
 import "./BGText.css";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * Use this component to create a repeated text background.
@@ -6,7 +8,37 @@ import "./BGText.css";
  * @param {import("react").ComponentProps} props
  */
 const BGText = props => {
-	return <div></div>;
+	const { cols, rows = 1, text } = props;
+
+	const textRows = [];
+	for (let i = 0; i < rows; i++) {
+		const textCols = [];
+		for (let j = 0; j < cols; j++) {
+			textCols.push(
+				<span className="text-subtitle BGT-text" key={uuidv4()}>
+					{text}
+				</span>
+			);
+		}
+
+		textRows.push(
+			<div className="BGT-row" key={uuidv4()}>
+				{textCols}
+			</div>
+		);
+	}
+
+	return (
+		<div className="BGT">
+			{textRows}
+		</div>
+	);
+};
+
+BGText.propTypes = {
+	cols : PropTypes.number.isRequired,
+	rows : PropTypes.number.isRequired,
+	text : PropTypes.string.isRequired
 };
 
 export default BGText;
