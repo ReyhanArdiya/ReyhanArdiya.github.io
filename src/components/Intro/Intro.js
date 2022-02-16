bda2b38106624099b958ef9b85ecc017e948f801
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-16 11:30:36 +0700

    Style main intro text

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,7 +4,7 @@ const Intro = props => {
 	return (
 		<section id="intro">
 			<p className="text-subtitle">Hey! I'm,</p>
-			<h1 className="text-title">REYHAN</h1>
+			<h1 className="text-title"><span>REY</span><span>HAN</span></h1>
 			<p className="text-subtitle">I am a...</p>
 		</section>
 	);
