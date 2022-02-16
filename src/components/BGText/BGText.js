408d1df16106f890d488fdc2cdb71f59b9c5cda8
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-16 11:33:59 +0700

    Create `BGText.js` components

---
+++ src/components/BGText/BGText.js
@@ -0,0 +1,12 @@
+import "./BGText.css";
+
+/**
+ * Use this component to create a repeated text background.
+ *
+ * @param {import("react").ComponentProps} props
+ */
+const BGText = props => {
+	return <div></div>;
+};
+
+export default BGText;
