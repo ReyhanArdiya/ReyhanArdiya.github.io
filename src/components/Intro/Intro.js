cb9eed849bc85d76c4b5656a3cc31a77736d8fca
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-16 11:00:44 +0700

    Add starting Intro code

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,13 @@
 import "./Intro.css";
 
 const Intro = props => {
-	return <div></div>;
+	return (
+		<section id="intro">
+			<p className="text-subtitle">Hey! I'm,</p>
+			<h1 className="text-title">REYHAN</h1>
+			<p className="text-subtitle">I am a...</p>
+		</section>
+	);
 };
 
 export default Intro;
