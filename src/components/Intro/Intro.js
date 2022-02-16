2505220fd555e0248224ee20bd04757f3c767b64
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-16 21:05:02 +0700

    Add `BGText` for `#intro`

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,16 @@
 import "./Intro.css";
+import BGText from "../BGText/BGText.js";
 
-const Intro = props => {
+const Intro = () => {
 	return (
 		<section id="intro">
 			<p className="text-subtitle">Hey! I'm,</p>
-			<h1 className="text-title"><span>REY</span><span>HAN</span></h1>
+			<div className="text-title">
+				<BGText cols={9} rows={4} text="HEY"/>
+				<h1 >
+					<span>REY</span><span>HAN</span>
+				</h1>
+			</div>
 			<p className="text-subtitle">I am a...</p>
 		</section>
 	);
