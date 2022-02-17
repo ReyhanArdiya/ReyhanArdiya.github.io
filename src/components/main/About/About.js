c354499c63244ece7fb153c7ab716ab08d465805
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-17 22:28:32 +0700

    Use common Text components in About

--- src/components/main/About/About.js
+++ src/components/main/About/About.js
@@ -1,15 +1,16 @@
 import "./About.css";
 import BGText from "../../common/BGText/BGText.js";
+import TitleSideways from "../../common/Text/TitleSideways.js";
 
 const About = () => {
 	return (
 		<section id="about">
 			<BGText cols={3} rows={3} text="WEB DEVELOPER"/>
-			<header className="text-title-sideways">Who am i</header>
+			<TitleSideways id="about-header">Who am i</TitleSideways>
 			<section id="about-text">
-				<h2 className="text-title">WEB DEVELOPER</h2>
+				<h2 id="about-title-one" className="text-title">WEB DEVELOPER</h2>
 				<small className="text-subtitle">in</small>
-				<h2 className="text-title">TRAINING</h2>
+				<h2 id="about-title-two" className="text-title">TRAINING</h2>
 			</section>
 			<BGText cols={5} rows={3} text="TRAINING"/>
 		</section>
