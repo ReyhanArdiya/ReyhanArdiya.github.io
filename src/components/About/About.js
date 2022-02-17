01c39c5f622c57d1cad88a24136f015d4b53bfc6
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-17 14:15:11 +0700

    Add `BGT` to `#about`

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,17 @@
 import "./About.css";
-// import BGText from "../BGText/BGText.js";
+import BGText from "../BGText/BGText.js";
 
-// BGText;
 const About = () => {
 	return (
 		<section id="about">
+			<BGText cols={3} rows={3} text="WEB DEVELOPER"/>
 			<header className="text-title-sideways">About</header>
 			<section id="about-text">
 				<h2 className="text-title">WEB DEVELOPER</h2>
 				<small className="text-subtitle">in</small>
 				<h2 className="text-title">TRAINING</h2>
 			</section>
+			<BGText cols={5} rows={3} text="TRAINING"/>
 		</section>
 	);
 };
