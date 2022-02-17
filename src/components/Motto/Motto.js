4ad357cddc6cd5bbbd80ec480193118fa9f5ae7c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-17 15:19:13 +0700

    Add and style ` #motto` BGT

--- src/components/Motto/Motto.js
+++ src/components/Motto/Motto.js
@@ -1,10 +1,12 @@
 import "./Motto.css";
+import BGText from "../BGText/BGText.js";
 
-const Motto = props => {
+const Motto = () => {
 	return (
 		<section id="motto">
 			<p className="text-subtitle">My motto is to...</p>
 			<article id="motto-text">
+				<BGText cols={4} rows={8} text="EVERYDAY" />
 				<h2 className="text-title">KEEP</h2>
 				<h3 className="text-title">ON <span>LEARNING</span></h3>
 			</article>
