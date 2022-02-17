7821fcd847502bff5ca9552729605e4030b6eb9c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-17 11:56:00 +0700

    Create and style `About`

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import "./About.css";
+// import BGText from "../BGText/BGText.js";
+
+// BGText;
+const About = () => {
+	return (
+		<section id="about">
+			<header className="text-title-sideways">About</header>
+			<section id="about-text">
+				<h2 className="text-title">WEB DEVELOPER</h2>
+				<small className="text-subtitle">in</small>
+				<h2 className="text-title">TRAINING</h2>
+			</section>
+		</section>
+	);
+};
+
+export default About;
