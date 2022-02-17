67c8cebc8808a5e8fc97a7cc30c885da54800520
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-17 14:31:48 +0700

    Style some Motto

--- src/components/Motto/Motto.js
+++ src/components/Motto/Motto.js
@@ -1,7 +1,15 @@
 import "./Motto.css";
 
 const Motto = props => {
-	return <section id="motto"></section>;
+	return (
+		<section id="motto">
+			<p className="text-subtitle">My motto is to...</p>
+			<article id="motto-text">
+				<h2 className="text-title">KEEP</h2>
+				<h3 className="text-title">ON <span>LEARNING</span></h3>
+			</article>
+		</section>
+	);
 };
 
 export default Motto;
