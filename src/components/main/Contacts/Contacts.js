81372d8f788c8c323ae870b57ebf39511bfaff0a
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-19 20:59:43 +0700

    Style Contacts intro

--- src/components/main/Contacts/Contacts.js
+++ src/components/main/Contacts/Contacts.js
@@ -1,5 +1,33 @@
+import Subtitle from "../../common/Text/Subtitle.js";
+import styled from "styled-components";
+
+const ContactsContainer = styled.section`
+    grid-area: contacts;
+    width: 100%;
+    height: 200px;
+`;
+
+const ContactsIntro = styled(Subtitle)`
+    text-align: left;
+    margin-left: 2em;
+    margin-bottom: 1.85em;
+`;
+
+const ContactsContent = styled.section`
+    width: 100%;
+    background-color: red;
+    height: 100%;
+`;
+
 const Contacts = ({ propsDestruct }) => {
-	return <section id="contacts"></section>;
+	return (
+		<ContactsContainer id="contacts">
+			<ContactsIntro>If you want to contact me...</ContactsIntro>
+			<ContactsContent>
+
+			</ContactsContent>
+		</ContactsContainer>
+	);
 };
 
 export default Contacts;
