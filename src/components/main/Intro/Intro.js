909c436e9d05c62d47419e80201f964af93b5f84
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-21 14:41:17 +0700

    Animate `#intro`

--- src/components/main/Intro/Intro.js
+++ src/components/main/Intro/Intro.js
@@ -1,9 +1,27 @@
 import "./Intro.css";
 import BGText from "../../common/BGText/BGText.js";
+import styled from "styled-components";
+import { useInView } from "react-intersection-observer";
 
+const Container = styled.section`
+	transition: all 0.4s ease-in;
+	transform: translateY(${({ visible }) => visible ? "0" : "1rem"});
+	opacity: ${({ visible }) => visible ? "1" : "0"};
+`;
+
+let isAnimated = false;
 const Intro = () => {
+	const { ref, inView } = useInView({
+		root      : null,
+		threshold : 0.1
+	});
+
+	if (inView) {
+		isAnimated = true;
+	}
+
 	return (
-		<section id="intro">
+		<Container id="intro" ref={ref} visible={isAnimated}>
 			<p className="text-subtitle">Hey! I'm,</p>
 			<div className="text-title">
 				<BGText cols={9} rows={4} text="HEY"/>
@@ -12,7 +30,7 @@ const Intro = () => {
 				</h1>
 			</div>
 			<p className="text-subtitle">I am a...</p>
-		</section>
+		</Container>
 	);
 };
 
